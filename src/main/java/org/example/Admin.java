f032c75b623aa5582d2e160f2c83a487814a0596
saurabhg1117 <saurabhg1117@gmail.com>
2023-07-29 18:18:22 +0530

    ParkingLot

---
+++ src/main/java/org/example/Admin.java
@@ -0,0 +1,7 @@
+package org.example;
+
+
+public class Admin {
+    String name;
+
+}
---
+++ src/main/java/org/example/DisplayBoard.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class DisplayBoard {
+    public Status status;
+
+    public DisplayBoard() {
+        this.status = Status.AVAILABLE;
+    }
+
+    public void update(Status newStatus){
+        this.status = newStatus;
+    }
+}
+
